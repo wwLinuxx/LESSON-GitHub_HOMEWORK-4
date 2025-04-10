39d58031d6fc4e6b6afe93ec7e68325fe73315e5
wwlinuxoff@gmail.com <wwlinuxoff@gmail.com>
2025-04-10 18:43:14 +0500

    9

--- LESSON-GitHub_HOMEWORK-4/Program.cs
+++ LESSON-GitHub_HOMEWORK-4/Program.cs
@@ -1,8 +1,1 @@
-﻿Console.WriteLine("1");
-Console.WriteLine("2");
-Console.WriteLine("3");
-Console.WriteLine(5);
-Console.WriteLine(5);
-Console.WriteLine(6);
-Console.WriteLine(7);
-Console.WriteLine(8);
+﻿Console.WriteLine(9);
